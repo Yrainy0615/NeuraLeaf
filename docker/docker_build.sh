85e5e343a75f78492e51cb35548db08a73ae98e8
Yrainy0615 <yrainy0615@outlook.com>
2024-05-25 17:37:15 +0900

    update docker

---
+++ docker/docker_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+username=yyang
+docker build -t repo-luna.ist.osaka-u.ac.jp:5000/${username}/neuraleaf:build .
---
+++ docker/docker_run.sh
@@ -0,0 +1,2 @@
+username=yyang
+docker run --gpus all -itd --rm -u $(id -u $username):$(id -g $username) --name ${username}_neuraleaf -v /mnt/workspace2024/${username}/NeuraLeaf:/home/${username}/mnt/workspace/ repo-luna.ist.osaka-u.ac.jp:5000/${username}/neuraleaf:build bash
